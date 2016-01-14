5d9bf477bcc1e0aece70d754960390a33610fc2b
Paul Tournemaine <p.tournemaine@a2si-identit.fr>
2016-01-14 13:52:42 +0100

    ADD : middleware ajax ADD : public controller pour les actions autorisées public ADD : utilisation de la recherche en ajax pour l'autocompletion CHANGE : format des templates avec netbeans pour la mise en forme CHANGE : upgrade autocomplete library version

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,23 +1,27 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+  |--------------------------------------------------------------------------
+  | Application Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register all of the routes for an application.
+  | It's a breeze. Simply tell Laravel the URIs it should respond to
+  | and give it the controller to call when that URI is requested.
+  |
+ */
 
-Route::get('/', 'WelcomeController@Index');
-Route::get('/home', 'WelcomeController@Index');
-Route::get('infos', 'InformationController@Infos');
+Route::get('/', 'PublicController@anyIndex');
+Route::get('/home', 'PublicController@anyIndex');
+
+// Controller public pour les interraction publiques
+Route::controller('public', 'PublicController', [
+    "anySearchEntity" => "public.search",
+]);
 
 Route::get('admin/users', 'UserController@Index');
 Route::get('admin/users/{users}/editPW', array('as' => 'admin.users.editPW', 'uses' => 'UserController@editPW'));
-Route::post('admin/users/{users}', array('as' => 'admin.users.updatePW', 'uses' => 'UserController@updatePW') );
+Route::post('admin/users/{users}', array('as' => 'admin.users.updatePW', 'uses' => 'UserController@updatePW'));
 
 // Authentication routes...
 Route::get('auth/login', 'Auth\AuthController@getLogin');
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,35 +1,94 @@
 @extends('template')
 
 @section('header')
-    <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/style.css') }}">
-    <title>AXIS-MOM</title>
+<link rel="stylesheet" type="text/css" href="{{ URL::asset('css/style.css') }}">
+<title>AXIS-MOM</title>
+<script type="text/javascript">
+    function getGlyphicon(cat) {
+	switch (cat) {
+	    case 'Activite':
+		return 'wrench';
+	    case 'Evènement':
+		return 'time';
+	    case 'Lieu':
+		return 'home';
+	    case 'Organisation':
+		return 'education';
+	    case 'Objet':
+		return 'book';
+	    case 'Personne':
+		return 'user';
+	    default:
+		return 'tag';
+	}
+    }
+
+    $.widget("custom.catcomplete", $.ui.autocomplete, {
+	_create: function () {
+	    this._super();
+	    this.widget().menu("option", "items", "> :not(.ui-autocomplete-category)");
+	},
+	_renderMenu: function (ul, items) {
+	    ul.addClass('list-group col-sm-3');
+	    var that = this,
+		    currentCategory = "";
+	    $.each(items, function (index, item) {
+
+		if (item.category != currentCategory) {
+		    ul.append("<li class='list-group-item ui-autocomplete-category'><span class='glyphicon glyphicon-" + getGlyphicon(item.category) + "'></span> " + item.category + "</li>");
+		    currentCategory = item.category;
+		}
+		" activite=> wrench , event=> time , lieu => home , organisation => education, objet => book, personne => user"
+		var li = that._renderItemData(ul, item);
+		li.addClass('list-group-item');
+
+	    });
+	},
+	messages: {
+	    noResults: '',
+	    results: function () {
+	    }
+	},
+    });
+
+    $(document).ready(function () {
+	$('#welcomeAutoComplete').catcomplete({
+	    source: function (request, response) {
+		$.getJSON("{{route('public.search')}}", {needle: request.term}, response);
+	    },
+	    messages: {
+		noResults: '',
+		results: function () {
+		}
+	    },
+	    delay: 600
+	});
+    });
+</script>
 @stop
 
 @section('title')
-    AXIS-MOM
+AXIS-MOM
 @stop
 
 @section('contenu')
-                <div class="row">
-                <div class="col-sm-6 col-sm-offset-3">
-                    <form action="{{url('infos')}}" method="GET">
-                        <div id="imaginary_container"> 
-                            
-                            <div id="blocSearch">
-                                <div class="input-group stylish-input-group">
-                                    <input type="text" class="form-control" id="autocomplete" placeholder="Rechercher une oeuvre ou un artiste" >
-                                    <span class="input-group-addon">
-                                        <button type="submit">
-                                            <span class="glyphicon glyphicon-search"></span>
-                                        </button>  
-                                    </span>
-                                    
-                                </div>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-                </div>
+<div class="row">
+    <div class="col-sm-6 col-sm-offset-3">	
+	<div id="imaginary_container"> 
+	    <div id="blocSearch">
+		<div class="input-group stylish-input-group">
+		    <input type="text" class="form-control" id="welcomeAutoComplete" placeholder="Rechercher une oeuvre ou un artiste" >
+		    <span class="input-group-addon">
+			<button type="submit">
+			    <span class="glyphicon glyphicon-search"></span>
+			</button>  
+		    </span>
+
+		</div>
+	    </div>
+	</div>
+    </div>
+</div>
 @stop
 
 @section('footer')
