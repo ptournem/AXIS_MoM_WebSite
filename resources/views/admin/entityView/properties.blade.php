99a21fd2c2a3c22f255309a16392cb9da39c1a89
Corentin HATON <corentin.haton@ig2i.fr>
2016-02-08 21:57:27 +0100

    + FiX : SearchEntities

--- resources/views/admin/entityView/properties.blade.php
+++ resources/views/admin/entityView/properties.blade.php
@@ -59,7 +59,7 @@
                         @else
                         <span class="hidden" style="display: none">{{ $retour->value_locale }}</span>
 			<div class="input-group">
-			    <span contenteditable="true" class="value value-edited searchEntities form-control"/>{{ $retour->value_locale }}</span>                      
+			    <span contenteditable="true" name="{{ $retour->name }}" class="value value-edited searchEntities form-control"/>{{ $retour->value_locale }}</span>                      
 			    <div class="input-group-btn" role="group">
 				<button type="button"  name="{{ $retour->name }}" class="btn btn-warning btn-warning-locale btn-warning-name-{{ $retour->name }} disabled">
 				    <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
